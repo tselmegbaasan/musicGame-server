8aa3f65c0f984133dc6683a537251690369f5b2c
Tselmeg Baasan <ts2530ba-s@student.lu.se>
2021-04-24 19:13:08 +0200

    pushing to deploy

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: '../../db.sqlite'
+    storage: './db.sqlite'
 });
 
 const db = {};
